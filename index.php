a06f43e2fc68b9cfe0af50c9c21fdd684e18cb7b
luunavasv <43542934+luunavasv@users.noreply.github.com>
2020-06-24 15:01:30 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,258 @@
+<?php
+require_once('../../core/helpers/template-commerce.php');
+Page::headerTemplate('Lost Sock');
+?>
+                <div class="cover-sizing bg-image">
+                    <div class="product-info">
+                        <h1 class="text-secondary font-weight-bold">BEE SOCK</h1>
+                        <p class="text-secondary">Lorem ipsum, dolor sit amet consectet</p>
+                        <h2 class="text-secondary mb-5">$12.99</h2>
+                        <button type="button" class="btn btn-primary custom--button">Comprar</button>
+                    </div>
+                </div>
+            
+                <!--Slider de productos-->
+                <div class="product-slider">
+                    <div class="product-slider-container mb-3">
+                        <!--Contenido del primer slider-->
+                        <div class="card product-slider-card">
+                            <div class="row no-gutters">
+                                <div class="col-5">
+                                    <img src="../../resources/img/socks.jpg" class="card-img" alt="...">
+                                </div>
+                                <div class="col-7">
+                                    <div class="card-body">
+                                        <div class="slider--card">
+                                            <p class="card-title text-secondary font-weight-bold">Banana Socks</p>
+                                                <div class="star--card">
+                                                    <span class="fa fa-star text-yellow"></span>
+                                                    <span class="fa fa-star text-yellow"></span>
+                                                    <span class="fa fa-star text-yellow"></span>
+                                                    <span class="fa fa-star text-yellow"></span>
+                                                    <span class="fa fa-star text-yellow"></span>
+                                                </div>
+                                            <p class="card-text"><small class="text-purple font-weight-bold"">$5.00</small></p>
+                                        </div>                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--Contenido del primer slider-->
+                        <div class="card product-slider-card">
+                            <div class="row no-gutters">
+                                <div class="col-5">
+                                    <img src="../../resources/img/socks.jpg" class="card-img" alt="...">
+                                </div>
+                                <div class="col-7">
+                                    <div class="card-body">
+                                        <div class="slider--card">
+                                            <p class="card-title text-secondary font-weight-bold">Banana Socks</p>
+                                            <div class="star--card">
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                            </div>
+                                            <p class="card-text"><small class="text-purple font-weight-bold"">$5.00</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--Contenido de primer slider-->
+                        <div class="card product-slider-card">
+                            <div class="row no-gutters">
+                                <div class="col-5">
+                                    <img src="../../resources/img/socks.jpg" class="card-img" alt="...">
+                                </div>
+                                <div class="col-7">
+                                    <div class="card-body">
+                                        <div class="slider--card">
+                                            <p class="card-title text-secondary font-weight-bold">Banana Socks</p>
+                                            <div class="star--card">
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                            </div>
+                                            <p class="card-text"><small class="text-purple font-weight-bold"">$5.00</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+                        <!--Contenido del primer slider-->
+                        <div class="card product-slider-card">
+                            <div class="row no-gutters">
+                                <div class="col-5">
+                                    <img src="../../resources/img/socks.jpg" class="card-img" alt="...">
+                                </div>
+                                <div class="col-7">
+                                    <div class="card-body">
+                                        <div class="slider--card">
+                                            <p class="card-title text-secondary font-weight-bold">Banana Socks</p>
+                                            <div class="star--card">
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                            </div>
+                                            <p class="card-text"><small class="text-purple font-weight-bold"">$5.00</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--Contenido del primer slider-->
+                        <div class="card product-slider-card">
+                            <div class="row no-gutters">
+                                <div class="col-5">
+                                    <img src="../../resources/img/socks.jpg" class="card-img" alt="...">
+                                </div>
+                                <div class="col-7">
+                                    <div class="card-body">
+                                        <div class="slider--card">
+                                            <p class="card-title text-secondary font-weight-bold">Banana Socks</p>
+                                            <div class="star--card">
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                                <span class="fa fa-star text-yellow"></span>
+                                            </div>
+                                            <p class="card-text"><small class="text-purple font-weight-bold"">$5.00</small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--Slider de productos-->
+    
+                <!--Section que contiene la información de relevancia para los compradores-->
+                <section class="container-fluid my-5">
+                    <div class="row justify-content-center no-gutters">
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-3 mb-5 mb-md-0">
+                                <div class="card text-center custom--card">
+                                    <div class="card-body">
+                                        <div class="fad fa-heart mb-5 fa-3x text-light-blue"></div>
+                                        <h4 class="card-title mb-4 text-secondary font-weight-light">Diseños únicos</h2>
+                                        <p class="card-text mx-auto text-secondary">Diseños creados especialmente para representar
+                                            a cada personalidad, y a aquella personalidad alocada que todos llevamos
+                                            dentro.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-3 mb-5 mb-md-0">
+                                <div class="card text-center custom--card">
+                                    <div class="card-body">
+                                        <div class="fad fa-piggy-bank mb-5 fa-3x text-light-blue"></div>
+                                        <h4 class="card-title mb-4 text-secondary font-weight-light">Precios asequibles</h2>
+                                        <p class="card-text mx-auto text-secondary">Porque pensamos en la economía y
+                                            las posibilidades de nuestros clientes especiales, contamos con
+                                            precios desde $5 dólares.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-3 mb-5 mb-md-0">
+                                <div class="card text-center custom--card">
+                                    <div class="card-body">
+                                        <div class="fad fa-truck mb-5 fa-3x text-light-blue"></div>
+                                        <h4 class="card-title mb-4 text-secondary font-weight-light">Envíos a todo el país</h2>
+                                        <p class="card-text mx-auto text-secondary">Envíos rápidos y económicos a cada rincón de país.
+                                        Entregas hasta la puerta de tu casa, tu oficina o centros comerciales.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-3 mb-5 mb-md-0">
+                                <div class="card text-center custom--card">
+                                    <div class="card-body">
+                                        <div class="fad fa-heart mb-5 fa-3x text-light-blue"></div>
+                                        <h4 class="card-title mb-4 text-secondary font-weight-light">Diseños únicos</h2>
+                                        <p class="card-text mx-auto text-secondary">Diseños creados especialmente para representar
+                                            a cada personalidad, y a aquella personalidad alocada que todos llevamos
+                                            dentro.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--Card-->
+                    </div>
+                </section>
+                <hr>
+                <!--Section que contiene el apartado descubrir de las nuevas colecciones-->
+                <section class="container-fluid my-5">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-sm-6 mx-auto">
+                            <img src="../../resources/img/back.jfif" alt="..." class="float-right w-75" id="discover">
+                        </div>  
+                        <div class="col-12 col-sm-6 text-center justify-content-center my-auto">
+                            <h3 class="text-secondary">BEE SOCK</h3>
+                            <p class="text-secondary">Encuentra los calcetines que más te representan con nuestra nueva colección de animales</p>
+                            <button type="button" class="btn btn-primary custom--button">Comprar</button>
+                        </div>  
+                    </div>
+                </section>
+                <hr>
+                <div class="container mb-5">
+                    <h3 class="text-center my-5 text-secondary">Exclusivos</h3>
+                    <div class="row">
+                        <div class="owl-carousel owl-theme">
+                            <div class="item">
+                                <div class="card custom--card">
+                                    <img class="card-img-top img-fluid" src="../../resources/img/socks3.jpg" alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title text-center">Penguin Sock</h5>
+                                        <p class="card-text text-center">$5</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="card custom--card">
+                                    <img class="card-img-top img-fluid" src="../../resources/img/socks3.jpg" alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title text-center">Penguin Sock</h5>
+                                        <p class="card-text text-center">$5</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="card custom--card">
+                                    <img class="card-img-top img-fluid" src="../../resources/img/socks3.jpg" alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title text-center">Penguin Sock</h5>
+                                        <p class="card-text text-center">$5</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="card custom--card">
+                                    <img class="card-img-top img-fluid" src="../../resources/img/socks3.jpg" alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title text-center">Penguin Sock</h5>
+                                        <p class="card-text text-center">$5</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="card custom--card">
+                                    <img class="card-img-top img-fluid" src="../../resources/img/socks3.jpg" alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title text-center">Penguin Sock</h5>
+                                        <p class="card-text text-center">$5</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+<?php
+Page::footerTemplate();
+?>
